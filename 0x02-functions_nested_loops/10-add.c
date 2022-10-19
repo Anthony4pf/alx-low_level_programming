2561cb95cfd1fe026e1819d889b92ff323cd66d6
Anthony Ede <edeanthony14@gmail.com>
2022-10-19 15:24:57 -0700

    Summation of numbers in c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*add - Add two integers and print out the results
+*@x: first number
+*@y: second number
+*Return: result of the summation
+*/
+int add(int x, int y)
+{
+	int sum;
+
+	sum = x + y;
+
+	return (sum);
+}

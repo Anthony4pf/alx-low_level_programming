0f8f8380e3c257ac389d6492b46a17f30ab1036d
Anthony Ede <edeanthony14@gmail.com>
2022-10-24 16:19:49 -0700

    Add Script

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+*puts_half - Print half of the string
+*@str: String to be halfed
+*Return: void always
+*/
+
+void puts_half(char *str)
+{
+	int length, n, i;
+
+	for (length = 0; str[length] != '\0'; length++)
+	{
+
+	}
+
+	n = (length - 1) / 2;
+
+	for (i = n + 1; str[i] != '\0'; i++)
+	{
+		_putchar (str[i]);
+	}
+
+		_putchar ('\n');
+}

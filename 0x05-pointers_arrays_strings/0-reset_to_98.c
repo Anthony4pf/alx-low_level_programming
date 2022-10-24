56f02fa6b053ec5fc366fd7556957321b2f269ae
Anthony Ede <edeanthony14@gmail.com>
2022-10-24 13:05:27 -0700

    Added script

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*reset_to_98 - reset entered integer to 98
+*@n: Number to be reset
+*Return: Always 0
+*/
+
+void reset_to_98(int *n)
+{
+	int i = 98;
+
+	*n = &i;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+Pointers is a special type of variable that points to the address of a piece of data in memory.
+This module is an introduction to Pointers, Arrays and String.
+

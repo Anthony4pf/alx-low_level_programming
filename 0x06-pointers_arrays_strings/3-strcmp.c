d931bcdfba08dd8d6a84d24987b9a24de4ca6090
Anthony Ede <edeanthony14@gmail.com>
2022-10-26 16:30:01 -0700

    Add script after testing

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+*_strcmp - Compare two strings and return a value after comparison
+*@s1: first string
+*@s2: second string
+*Return: Negative if the numbers
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] < s2[i])
+			return (-15);
+
+		if (s1[i] > s2[i])
+			return (15);
+
+		if (s2[i] == s2[i])
+			return (0);
+
+	}
+}

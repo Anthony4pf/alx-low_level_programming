fc5951e138cf010f7b3e089ff3cb5c21c9cbb100
Anthony Ede <edeanthony14@gmail.com>
2022-10-26 16:01:08 -0700

    Update to Script

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,18 +9,18 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
+	int i, j, length;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	for (length = 0; dest[length] != '\0'; length++)
 	{
 	}
 	
-	for (j = 0; src[j] != '\0' && n--; j++, i++)
+	for (i, j= 0; src[j] != '\0' && i < n; i++, j++)
 	{
-		dest[i] = src[j];
+		dest[length + i] = src[j];
 	}
 
-	_putchar('\0');
+	dest[length + i] = '\0';
 
 	return (dest);
 }

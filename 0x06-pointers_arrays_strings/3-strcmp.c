4cd702b3bf69d59ae06fa0ea04cde689290396c1
Anthony Ede <edeanthony14@gmail.com>
2022-10-27 15:01:58 -0700

    Added New script for Debugging

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,18 +9,17 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int res = 0;
 
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	while (*s1 == *s2 && *s1 != '\0')
 	{
-		if (s1[i] < s2[i])
-			return (-15);
+		s1++;
 
-		else if (s1[i] > s2[i])
-			return (15);
-
-		else if (s1[i] == s2[i])
-			return (0);
-
+		s2++;
 	}
+
+	if (s1 != s2)
+		res = *s1 - *s2;
+
+	return (res);
 }

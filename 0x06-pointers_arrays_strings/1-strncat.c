a7e0a8053f80f097bef671d41cd75eed3f48c694
Anthony Ede <edeanthony14@gmail.com>
2022-10-27 14:39:28 -0700

    Test New Fix for Script

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,26 +1,26 @@
 #include "main.h"
+#include <string.h>
 
 /**
 *_strncat - concatenates two strings with at most n bytes from src
 *@dest: first string
 *@src: second string
+*@n: number
 *Return: a pointer to dest
 */
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j, length;
+	int length, i;
 
-	for (length = 0; dest[length] != '\0'; length++)
+	length = strlen(dest)
+
+	for (i = 0; i < n && src[i]; src++, i++)
 	{
-	}
-	
-	for (i, j= 0; src[j] != '\0' && i < n; i++, j++)
-	{
-		dest[length + i] = src[j];
+		dest[length + i] = src[i];
 	}
 
-	dest[length + i] = '\0';
+		dest[length + i] = '\0';
 
-	return (dest);
+		return (dest);
 }

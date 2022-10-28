e3423e99201d260bdb94b716b6911b5c46293cb2
Anthony Ede <edeanthony14@gmail.com>
2022-10-28 12:15:14 -0700

    Added Fix

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+*leet - encodes a string into 1337
+*@s: string to encode
+*Return: the encoded string
+*/
+
+char *leet(char *s)
+{
+	char str[] = "oOlLeEaAtT";
+	char num[] = "0011334477";
+
+	int i, j, k;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0, k = 0; str[j] != '\0'; j++)
+		{
+			if (s[i] == str[j]
+			{
+				k = j;
+				s[i] = num[k];
+			}
+		}
+	}
+
+	return (s);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,3 +6,4 @@ char *string_toupper(char *);
 char *cap_string(char *);
 void reverse_array(int *a, int n);
 char *_strncpy(char *dest, char *src, int n);
+char *leet(char *);

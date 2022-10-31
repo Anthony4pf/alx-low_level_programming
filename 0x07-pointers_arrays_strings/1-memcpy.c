ca9a690f34a0515db22a838954055d7d82fe07dd
Anthony Ede <edeanthony14@gmail.com>
2022-10-31 15:04:40 -0700

    Update Fix

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,11 +10,14 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i, j;
+	unsigned int i, j;
 
 	for (i = 0; i < n; i++)
 	{
-		dest[i] = src[i];
+		for (j = 0; src[j] != '\0'; j++)
+		{
+			dest[j] = src[j];
+		}
 	}
 
 	return (dest);

f88cc649c8828650b2959b0e04daf2e7921bc6c1
Anthony Ede <edeanthony14@gmail.com>
2022-10-31 13:54:48 -0700

    Added Script for First Check

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*_strchr - Locates a Chraracter in a string
+*@s: String
+*@c: Chracter to search for in the string
+*Return: *s
+*/
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+
+		s++;
+	}
+
+	return (0);
+}

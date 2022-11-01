4a1ee87d14dcb9c70618cf941b93e5fbf28b4708
Anthony Ede <edeanthony14@gmail.com>
2022-11-01 13:33:57 -0700

    added Files

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,3 +4,4 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 void set_string(char **s, char *to);
 void print_diagsums(int *a, int size);
 char *_strpbrk(char *s, char *accept);
+void print_chessboard(char (*a)[8]);

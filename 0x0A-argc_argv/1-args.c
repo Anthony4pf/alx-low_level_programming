090e862bfc6c0c710d711c458b1e73a027812304
Anthony Ede <edeanthony14@gmail.com>
2022-11-04 12:16:38 -0700

    Update Script

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+*main - prints the number of arguments
+*@argc: arguments count
+*@argv: arguments vector
+*Return: 0
+*/
+
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}	

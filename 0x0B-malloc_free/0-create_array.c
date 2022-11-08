4c3b4ea28c417fc09cf787549c71c16e3ab663ea
Anthony Ede <edeanthony14@gmail.com>
2022-11-08 15:14:52 -0800

    added file

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*create_array - create an array
+*@size: number of elements of the array
+*@c: the specific character in the array
+*Return: pointer
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *str = malloc(sizeof(char) * size);
+
+	if (size == 0)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		str[i] = c;
+
+	if (str == NULL)
+		return (NULL);
+	else
+		return (str);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+Introduction to Dynamic Memory allocation inn C using Malloc and Free.

d0043499ed9c7d2689600ed80df599af2a65315c
Anthony Ede <edeanthony14@gmail.com>
2022-11-11 14:03:52 -0800

    added New Files

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - multiply two arguments
+*@argc: argument counter
+*@argv: argument vector
+*Return: 0 (success)
+*/
+
+int main (int argc, char *argv[])
+{
+	unsigned long mul = 1;
+	unsigned int i, j;	
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit (98);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				exit (98);
+			}
+		}
+		
+		mul *= atoi(argv[i]);
+	}
+
+	printf("%lu\n", mul);
+
+	return (0);
+}

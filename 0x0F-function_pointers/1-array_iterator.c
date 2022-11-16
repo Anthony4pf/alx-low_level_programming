a8201888119140a35320c1f945f3c23cc6a51bce
Anthony Ede <edeanthony14@gmail.com>
2022-11-16 08:23:18 -0800

    Fixed betty style and doc error

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,7 +2,7 @@
 #include "function_pointers.h"
 
 /**
-*array_iterator - executes a function given as a parameter on each element of an arrayy
+*array_iterator - executes a function on each element of an array
 *@array: pointer to first element of the array
 *@size: size of the array
 *@action: function
@@ -11,13 +11,13 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-
 	unsigned int i;
+
 	if (action != NULL)
-	{ 
+	{
 		for (i = 0; i < size; i++)
 		{
-			action (array[i]);
+			action(array[i]);
 		}
 	}
 }

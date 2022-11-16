be8a6efd46cacd16954b98675f04d43e06118f17
Anthony Ede <edeanthony14@gmail.com>
2022-11-16 08:27:32 -0800

    Fix updates

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,13 +11,15 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i;
-
 	if (action != NULL)
 	{
+
+		unsigned int i;
+
 		for (i = 0; i < size; i++)
 		{
 			action(array[i]);
 		}
+
 	}
 }

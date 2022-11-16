0d2958b3087b787a1c58ec28f7f16890d3324aab
Anthony Ede <edeanthony14@gmail.com>
2022-11-16 08:01:12 -0800

    Add new file

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*print_name - print the name of a person according to specified function
+*@name: name to print
+*@f: pointer to function
+*Return: void
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,2 @@
+Intro to Function pointers in C.
+Function pointers are unique as they have the capability to store the initial adress of a function.

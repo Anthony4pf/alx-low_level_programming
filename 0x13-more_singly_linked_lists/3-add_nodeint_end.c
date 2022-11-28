9f5e6b2f2bec30e313e7d6b76a20d5c7112601a1
Anthony Ede <edeanthony14@gmail.com>
2022-11-28 15:12:49 -0800

    print node at the end of list

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+*add_nodeint_end - add a new node at the end of the list
+*@head: head node of the list
+*@n: integer value for the new node
+*Return: adress of new node
+*/
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *temp, *ptr;
+
+	temp = malloc(sizeof(listint_t));
+	if (temp == NULL)
+		return (NULL);
+
+	temp->n = n;
+	temp->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = temp;
+		return (NULL);
+	}
+
+	ptr = *head;
+
+	if (ptr->next != NULL)
+	{
+		ptr = ptr->next;
+	}
+
+	ptr->next = temp;
+
+	return (temp);
+}

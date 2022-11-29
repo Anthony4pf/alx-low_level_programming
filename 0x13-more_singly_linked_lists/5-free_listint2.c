19d652d72da52301db9c0ce53c6bf67b01602bb5
Anthony Ede <edeanthony14@gmail.com>
2022-11-29 07:21:10 -0800

    add new script to free list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+*free_listint2 - free a list and set head to NULL
+*@head: head node of the list
+*return: void
+*/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	temp = *head;
+
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		free(*head);
+		*head = temp;
+	}
+
+	*head = NULL;
+	temp = NULL;
+}

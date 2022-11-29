f9968658f70f87b026a96633dda96b1febae82fb
Anthony Ede <edeanthony14@gmail.com>
2022-11-29 13:40:35 -0800

    add new condition for while loop iteration

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,7 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		ptr = *head;
 
-		while (i < idx - 1)
+		while (i < idx - 1 && ptr != NULL)
 		{
 			ptr = ptr->next;
 			i++;

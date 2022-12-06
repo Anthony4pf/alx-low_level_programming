bc60c6fb803883f589aff2698e66c8c5320d13d5
Anthony Ede <edeanthony14@gmail.com>
2022-12-06 14:04:34 -0800

    Fix compilation errors

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 	if (fd_r < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		eixit(98); }
+		exit(98); }
 	x = read(fd_r, buf, BUFSIZ);
 	if (x == -1)
 	{

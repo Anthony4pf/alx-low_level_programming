1e2eacb0a99cd70461943fa48abf36d9c30e1085
Anthony Ede <edeanthony14@gmail.com>
2022-12-06 11:54:15 -0800

    added new flags

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s", argv[1]);
 		exit(98); }
-	fd_w = open(argv[2], O_CREAT | O_TRUNC | O_RDWR, 0664);
+	fd_w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_w < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);

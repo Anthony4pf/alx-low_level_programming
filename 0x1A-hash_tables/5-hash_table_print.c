b646025417ca510b3b837d2dcf8a6143d535f966
Anthony Ede <edeanthony14@gmail.com>
2023-02-07 08:04:02 -0800

    Add New File_5 after tests

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+
+/**
+*hash_table_print - prints a hash table in python dict format
+*@ht: hash table
+*Return: void
+*/
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *temp = NULL;
+	unsigned long int i;
+	int comma = 0;
+
+	if (ht == NULL)
+		return;
+
+	putchar('{');
+
+	for (i = 0; i < ht->size; i++)
+	{
+		temp = ht->array[i];
+		while (temp)
+		{
+			if (comma)
+				printf(", ");
+
+			printf("%s: %s", temp->key, temp->value);
+
+			if (comma == 0)
+				comma = 1;
+
+			temp = temp->next;
+		}
+	}
+	printf("}\n");
+}
